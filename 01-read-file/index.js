115d8fef15a81a9480e809007e5a0b87d906646f
pososhin <pososhin@gmail.com>
2021-11-03 03:46:57 +0300

    feat: 01-read-file

---
+++ 01-read-file/index.js
@@ -0,0 +1,9 @@
+var fs = require('fs');
+var stream = new fs.ReadStream("./01-read-file/text.txt",{encoding:"utf-8"});
+stream.on('readable', function(){
+    var data = stream.read();
+    if(data!==null) console.log(data);
+});
+stream.on('end', function(){
+    stream.destroy();
+});
