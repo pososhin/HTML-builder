2e0aa03d31609d17a7e827c461357b6f62b3323f
Посошин Дмитрий <C:\Users\dp\AppData\Roaming\The Bat!>
2021-11-05 07:47:52 +0300

    refactor: 04-copy-directory modul

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,5 +1,9 @@
-var fs = require('fs');
-const  check_version = require('../01-read-file/checkversion');
+// Вся логика вынесена в НЕ строний, а мой модуль cpfolder.js
+// Он тут же рядом. Не забудь проверить синхронные методы и там... 
+// А еще надо проверять на запрещеный метод fsPromises.cp() 
+// Это сделано, что бы с шестом задании не повторяться.
+const check_version = require('../01-read-file/checkversion');
+const copy_folder = require('./cpfolder').cp;
 
 const DIR = './04-copy-directory/'
 const DIR_SOURCE = 'files'
@@ -7,36 +11,4 @@ const DIR_TARGET = 'files-copy'
 
 check_version.valid();
 
-new Promise((res, rej) => {
-    fs.rm(DIR + DIR_TARGET, { recursive: true }, (e, r) => {
-        // if (e) rej(e);
-        fs.mkdir(DIR + DIR_TARGET, () => {
-            res();
-        });
-    });
-}).then((a) => {
-    return new Promise((res, rej) => {
-        function copyFiles(dir, source, target) {
-            fs.readdir(dir + source, (err, files) => {
-                if (err) rej(err);
-                for (let file of files) {
-                    fs.stat(dir + source + '/' + file, (errStat, status) => {
-                        if (errStat) rej(errStat);
-                        if (status.isFile()) {
-                            fs.createReadStream(dir + source + '/' + file)
-                                .pipe(fs.createWriteStream(dir + target + '/' + file));
-                        } else {
-                            fs.mkdir(dir + target + '/' + file, { recursive: true }, (e) => {
-                                if (e) rej(e);
-                                copyFiles(dir, source + '/' + file, target + '/' + file);
-                            });
-                        }
-                    });
-                }
-            });
-        }
-        copyFiles(DIR, DIR_SOURCE, DIR_TARGET);
-        res();
-    })
-}).catch((e) => console.log(e));
-
+copy_folder(DIR+DIR_SOURCE,DIR+DIR_TARGET);
--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -4,8 +4,8 @@ const check_version = require('../01-read-file/checkversion');
 
 // Вся логика вынесена в НЕ строний, а мой модуль bundlecss.js
 // Он тут же рядом. Не забудь проверить синхронные методы и там  
-// Это сделано, что бы с шестом задании не повторяться.
-const bundlecss = require('./bundlecss');
+// Это сделано, что бы в шестом задании не повторяться.
+const bundlecss = require('./bundlecss').bundle;
 
 check_version.valid();
 
@@ -13,4 +13,4 @@ const DIR = './05-merge-styles/';
 const DIR_STYLE = DIR + 'styles';
 const FILE_BUNDEL = DIR + 'project-dist/bundle.css';
 
-bundlecss.bundle(DIR_STYLE,FILE_BUNDEL);
+bundlecss(DIR_STYLE,FILE_BUNDEL);
